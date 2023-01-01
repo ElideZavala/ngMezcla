b466adfa9ef9473a0c26514a4c8f90c61ae81b88
ElideZavala <zavalavinagreelide@gmail.com>
2023-01-01 13:34:02 -0600

    ngMezcla de ngIf, ngSwitch, ngStyle y ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+@Component({ selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.scss'] })
 export class AppComponent {
   title = 'dirNgMezcla';
+  mostrar: boolean = false;
+  numeros: number[] = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6
+  ];
+  pares: boolean = true;
+  impares: boolean = true;
+  colorPar = 'blue';
+  colorImpar = 'red'
+
+  clActividad(valor: boolean) {
+    let classes = {
+      activo: valor,
+      inactivo: !valor
+    }
+
+    return classes
+  }
 }
